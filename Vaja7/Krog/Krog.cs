7ebfb420f4bc2751aba8d33d61985cbe09482425
petergreenfncs@gmail.com <petergreenfncs@gmail.com>
2025-11-14 09:35:40 +0100

    Dodana mapa Krog in razred Krog

---
+++ Vaja7/Krog/Krog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Projekt.Krog
+{
+    public class Krog
+    {
+        private double r;
+
+        // Privzeti konstruktor
+        public Krog()
+        {
+            r = 1;
+        }
+
+        // Konstruktor z argumentom
+        public Krog(double r)
+        {
+            this.r = r;
+        }
+
+        // Setter metoda
+        public void NastaviR(double r)
+        {
+            this.r = r;
+        }
+
+        // Obseg
+        public double Obseg()
+        {
+            return 2 * Math.PI * r;
+        }
+
+        // Ploščina
+        public double Ploscina()
+        {
+            return Math.PI * r * r;
+        }
+    }
+}

8dc6c52137c6a8bd8e0ae0a2ae9e63592f95fc87
Jurij LIPOVŠEK <jurijlipovsek@gmail.com>
2025-11-14 09:37:43 +0100

    Dodana mapa in razred Pravokotnik

---
+++ Vaja7/Pravokotnik/Pravokotnik.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaja7.Pravokotnik
+{
+    internal class Pravokotnik
+    {
+    }
+}

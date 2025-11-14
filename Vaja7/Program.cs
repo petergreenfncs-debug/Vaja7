2198b714c4372d943843baa4e96a5aa05fd5dcad
Jurij LIPOVŠEK <jurijlipovsek@gmail.com>
2025-11-14 09:57:21 +0100

    Dodal v Main program

--- Vaja7/Program.cs
+++ Vaja7/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vaja7.Pravokotnik;
 
 namespace Vaja7
 {
@@ -11,7 +12,12 @@ namespace Vaja7
         static void Main(string[] args)
         {
 
+            Pravokotnik p1 = new Pravokotnik(3, 3);
 
+            Pravokotnik p2 = new Pravokotnik(6, 4);
+
+            Console.WriteLine("Ploščina prvega pravokotnika: " + p1.Ploscina());
+            Console.WriteLine("Ploščina drugega pravokotnika: " + p2.Ploscina());
 
         }
     }
